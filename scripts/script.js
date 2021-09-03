c4cf8720b9c3a86a190a315a890f8eedf74f0b69
Hiron Tez <81362525+HironTez@users.noreply.github.com>
2021-09-03 22:51:56 +0300

    feat: add player rotation, change animation frames processing, add fps counter

--- scripts/script.js
+++ scripts/script.js
@@ -1,104 +1,108 @@
+let fps
+
 const header = $('header')
 const startMenu = $('#start-menu')
 const playBtn = $('#start-menu > #start')
+const fpsCounterElem = $('#fps-counter')[0]
 
 
 class Player {
-    constructor() {
+    constructor(maxVerticalSpeed = 20, acceleration = 0.5) {
         this.pos = 0
         this.verticalSpeed = 0
+        this.acceleration = acceleration
+        this.maxVerticalSpeed = maxVerticalSpeed
         this.incline = 0
         this.playerElem = $('#player')
         this.fly = false
-        this.gameRun = false
         this.touchesTheTopEdge = false
         this.touchesTheBottomEdge = false
 
 
         // Fly on screen press
         onpointerdown = () => {
-            if (this.gameRun) this.enableFly()
+            this.fly = true
         }
-        
+
         onpointerup = () => {
-            if (this.gameRun) this.disableFly()
+            this.fly = false
         }
     }
 
-    enablePhysics() {
+    physics() {
+        // Calculate acceleration relative to fps
+        const accelerationNow = shiftRelativeToFPS(this.acceleration)
+
         // Gravitation
-        this.gravitationIntervalId = setInterval((parentalThis) => {
-            if ((parentalThis.verticalSpeed > -10) && !parentalThis.fly && !parentalThis.touchesTheBottomEdge) parentalThis.verticalSpeed -= 1
-        }, 100, this);
-
+        if ((this.verticalSpeed > -this.maxVerticalSpeed) && !this.fly && !this.touchesTheBottomEdge) this.verticalSpeed -= accelerationNow
+        
         // Move player (inertia)
-        this.inertiaIntervalId = setInterval((parentalThis) => {
-            const screenHeight = $(window).height()
-            const oldPosition = Number(parentalThis.playerElem.css('top').match(/-?[0-9]+/)[0])
-            let newPosition = oldPosition - parentalThis.verticalSpeed
+        const screenHeight = $(window).height()
+        const oldPosition = Number(this.playerElem.css('top').match(/-?[0-9]+/)[0])
+        let newPosition = oldPosition - this.verticalSpeed
 
-            if (newPosition > screenHeight) {
-                newPosition = screenHeight
-                parentalThis.touchesTheBottomEdge = true
-                parentalThis.verticalSpeed = 0
-            } else if (newPosition < 0) {
-                newPosition = 0
-                parentalThis.touchesTheTopEdge = true
-                parentalThis.verticalSpeed = 0
-            } else {
-                parentalThis.touchesTheTopEdge = false
-                parentalThis.touchesTheBottomEdge = false
-            }
+        if (newPosition >= screenHeight) {
+            newPosition = screenHeight
+            this.touchesTheBottomEdge = true
+            this.verticalSpeed = 0
+        } else if (newPosition <= 0) {
+            newPosition = 0
+            this.touchesTheTopEdge = true
+            this.verticalSpeed = 0
+        } else {
+            this.touchesTheTopEdge = false
+            this.touchesTheBottomEdge = false
+        }
 
-            parentalThis.playerElem.css('top', `${newPosition}px`)
-        }, 1, this);
-    }
+        this.playerElem.css('top', `${newPosition}px`)
 
-    disablePhysics() {
-        clearInterval(this.gravitationIntervalId) // Stop gravitation
-        clearInterval(this.inertiaIntervalId) // Stop inertia
-    }
+        // Player inclination
+        this.playerElem.css('transform', `translateY(-50%) rotate(${-(60 / 100 * (this.verticalSpeed / this.maxVerticalSpeed * 100))}deg)`)
 
-    enableFly() {
         // Fly
-        this.fly = true
-        this.flyIntervalId = setInterval((parentalThis) => {
-            if ((parentalThis.verticalSpeed < 10) && !parentalThis.touchesTheTopEdge) parentalThis.verticalSpeed += 1
-        }, 100, this);
-    }
-
-    disableFly() {
-        this.fly = false
-        clearInterval(this.flyIntervalId) // Stop fly
+        if (this.fly && (this.verticalSpeed < this.maxVerticalSpeed) && !this.touchesTheTopEdge) this.verticalSpeed += accelerationNow
     }
 }
 
 
 class Game {
-    constructor(gravity = 10, horizontalSpeed = 1, HeightImitationSpeed = 5, backgroundImageSrc) {
+    constructor(gravity = 10, horizontalSpeed = 3, HeightImitationSpeed = 5, backgroundImageSrc) {
+        this.run = false
         this.gravity = gravity
         this.horizontalSpeed = horizontalSpeed
         this.HeightImitationSpeed = HeightImitationSpeed
+        this.fps = 0
         this.background = $('#game-background')
 
         this.background.css('background-image', `url(${backgroundImageSrc || "../images/textures/background/background.jpg"})`)
+        const backgroundImageSize = getBackgroundImageSize(this.background)
+        this.backgroundWidth = (backgroundImageSize[0] / 100 * ($(document).height() / backgroundImageSize[1] * 100))
     }
 
-    startMoveBackground() {
-        const backgroundImageWidth = getBackgroundImageSize(this.background)[0]
-
+    moveBackground() {
         // Move background
-        this.backgroundMoveIntervalId = setInterval((parentalThis) => {
-            const currentPosX = Number(parentalThis.background.css('background-position').match(/-?[0-9]+/)[0])
-            let posX = currentPosX - parentalThis.horizontalSpeed
-            if (posX <= -backgroundImageWidth) posX += backgroundImageWidth
-            parentalThis.background.css('background-position', posX + 'px 0');
-        }, 1, this);
+        const currentPosX = Number(this.background.css('background-position').match(/-?[0-9]+/)[0])
+        let newPosX = currentPosX - shiftRelativeToFPS(this.horizontalSpeed)
+        if (newPosX <= -this.backgroundWidth) newPosX += this.backgroundWidth
+        this.background.css('background-position', newPosX + 'px 0');
     }
 
-    stopMoveBackground() {
-        clearInterval(this.backgroundMoveIntervalId)
+    moveObstacle() {
+        for (const obstacle of $('.obstacle')) {
+
+        }
     }
+}
+
+
+const animationFrame = () => {
+    if (!game.run) return
+
+    player.physics()
+    game.moveBackground()
+
+    requestAnimationFrame(animationFrame)
+    fpsCounter()
 }
 
 
@@ -106,15 +110,12 @@ const game = new Game()
 const player = new Player()
 
 const startGame = () => {
-    game.startMoveBackground()
-    player.enablePhysics()
-    player.gameRun = true
+    game.run = true
+    requestAnimationFrame(animationFrame)
 }
 
 const stopGame = () => {
-    game.stopMoveBackground()
-    player.disablePhysics()
-    player.gameRun = false
+    game.run = false
 }
 
 
@@ -141,5 +142,5 @@ playBtn.on('click', () => {
         }
     })
 
-    setTimeout(startGame, 300); // Start game after 300 ms
+    setTimeout(startGame, 300) // Start game after 300 ms
 })
--- scripts/tools.js
+++ scripts/tools.js
@@ -1,4 +1,4 @@
-var getBackgroundImageSize = (object) => {
+const getBackgroundImageSize = (object) => {
     const imageSrc = object
         .css('backgroundImage').replace(/url\((['"])?(.*?)\1\)/gi, '$2')
         .split(',')[0]
@@ -9,4 +9,42 @@ var getBackgroundImageSize = (object) => {
     const width = image.width,
         height = image.height
     return [width, height]
+}
+
+
+let lastFrameTime
+
+// Calculate fps
+const fpsCounter = () => {
+    if (!lastFrameTime) {
+        lastFrameTime = Date.now()
+        fps = 0
+        return
+    }
+    delta = (Date.now() - lastFrameTime) / 1000
+    fps = 1 / delta
+
+    // Write value to html tag
+    if (Number(String(Date.now()).slice(-4, -3)) > Number(String(lastFrameTime).slice(-4, -3))) fpsCounterElem.innerText = `FPS: ${Math.round(fps)}`
+    lastFrameTime = Date.now() // Set new value
+}
+
+
+// Calculate the animation data relative to the fps
+const shiftRelativeToFPS = (data) => {
+    return fps ? data / 100 * (100 / fps * 100) : 0
+}
+
+
+// Function to check if any polet overlaps the given circle and rectangle
+const checkOverlap = (R, Xc, Yc, X1, Y1, X2, Y2) => {
+    // Find the nearest polet on the rectangle to the center of the circle
+    let Xn = Math.max(X1, Math.min(Xc, X2));
+    let Yn = Math.max(Y1, Math.min(Yc, Y2));
+       
+    // Find the distance between the nearest polet and the center of the circle
+    // Distance between 2 polets, (x1, y1) & (x2, y2) in 2D Euclidean space is ((x1-x2)**2 + (y1-y2)**2)**0.5
+    let Dx = Xn - Xc;
+    let Dy = Yn - Yc;
+    return (Dx * Dx + Dy * Dy) <= R * R;
 }
