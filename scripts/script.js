f6962b7878282c2f342dc2785cda701dbd3d4049
Hiron Tez <81362525+HironTez@users.noreply.github.com>
2021-09-09 13:43:35 +0300

    feat: add obstacles (asteroids) mehanic; add temporary skins and textures.

--- scripts/script.js
+++ scripts/script.js
@@ -8,7 +8,6 @@ const fpsCounterElem = $('#fps-counter')[0]
 
 class Player {
     constructor(maxVerticalSpeed = 20, acceleration = 0.5) {
-        this.pos = 0
         this.verticalSpeed = 0
         this.acceleration = acceleration
         this.maxVerticalSpeed = maxVerticalSpeed
@@ -35,7 +34,7 @@ class Player {
 
         // Gravitation
         if ((this.verticalSpeed > -this.maxVerticalSpeed) && !this.fly && !this.touchesTheBottomEdge) this.verticalSpeed -= accelerationNow
-        
+
         // Move player (inertia)
         const screenHeight = $(window).height()
         const oldPosition = Number(this.playerElem.css('top').match(/-?[0-9]+/)[0])
@@ -57,10 +56,24 @@ class Player {
         this.playerElem.css('top', `${newPosition}px`)
 
         // Player inclination
-        this.playerElem.css('transform', `translateY(-50%) rotate(${-(60 / 100 * (this.verticalSpeed / this.maxVerticalSpeed * 100))}deg)`)
+        this.playerElem.css('transform', `translateY(-50%) rotate(${-(60 / 100 * (this.verticalSpeed / this.maxVerticalSpeed * 100)) + 90}deg)`)
 
         // Fly
         if (this.fly && (this.verticalSpeed < this.maxVerticalSpeed) && !this.touchesTheTopEdge) this.verticalSpeed += accelerationNow
+    }
+}
+
+
+class Obstacle {
+    constructor(radius = 100, posY = 50, rotationSpeed) {
+        this.active = true
+        // Validate arguments (x = x < max ? max : (x > min ? x : min))
+        this.radius = radius > 200 ? 200 : (radius > 40 ? radius : 40)
+        this.posY = posY > 100 ? 100 : (posY > 0 ? posY : 0)
+        // Create new obstacle
+        this.obstacleElem = $('<img class="obstacle" src="./images/textures/asteroids/asteroid.png"></img>')
+        this.obstacleElem.css({ 'width': this.radius * 2, 'height': this.radius * 2, 'top': `${this.posY}%`, 'left': `calc(100% + ${this.radius}px)` }) // Set parameters
+        $('#game>#obstacles').append(this.obstacleElem) // Add to HTML
     }
 }
 
@@ -72,6 +85,7 @@ class Game {
         this.horizontalSpeed = horizontalSpeed
         this.HeightImitationSpeed = HeightImitationSpeed
         this.fps = 0
+        this.obstacles = []
         this.background = $('#game-background')
 
         this.background.css('background-image', `url(${backgroundImageSrc || "../images/textures/background/background.jpg"})`)
@@ -81,18 +95,76 @@ class Game {
 
     moveBackground() {
         // Move background
-        const currentPosX = Number(this.background.css('background-position').match(/-?[0-9]+/)[0])
+        const currentPosX = Number(this.background.css('background-position').match(/-?[0-9\.]+/)[0])
         let newPosX = currentPosX - shiftRelativeToFPS(this.horizontalSpeed)
         if (newPosX <= -this.backgroundWidth) newPosX += this.backgroundWidth
-        this.background.css('background-position', newPosX + 'px 0');
+        this.background.css('background-position', newPosX + 'px 0')
     }
 
-    moveObstacle() {
-        for (const obstacle of $('.obstacle')) {
+    generateObstacles() {
+        if (this.nextGenerateObstacleTime < Date.now() || !this.nextGenerateObstacleTime) {
+            const radius = random(40, 200)
+            const posY = random(0, 100)
+            this.obstacles.push(new Obstacle(radius, posY))
+            this.nextGenerateObstacleTime = Date.now() + random(500, 2000)
+        }
+    }
 
+    moveObstacles() {
+        for (const obstacle of this.obstacles) {
+            // Calculate new position
+            const currentPosX = Number(obstacle.obstacleElem.css('left').match(/-?[0-9\.]+/)[0])
+            const newPosX = currentPosX - shiftRelativeToFPS(this.horizontalSpeed)
+            // Remove the object if it disappeared off the screen
+            if (newPosX < (0 - (Number(obstacle.obstacleElem.css('width').match(/[0-9]+/)[0]) / 2))) {
+                obstacle.obstacleElem.remove()
+                obstacle.active = false
+            }
+            // Change position
+            obstacle.obstacleElem.css('left', `${newPosX}px`)
+        }
+    }
+
+    checkCollision() {
+        for (const obstacle of this.obstacles) {
+            if (!obstacle.active) { // Remove a obstacle from list if it's inactive
+                this.obstacles.splice($.inArray(obstacle, this.obstacles), 1)
+                continue
+            }
+            // Calculate the position of the obstacle where collision is possible
+            const collisionPosX = (
+                Number(player.playerElem.css('left').match(/[0-9\.]+/)[0]) -
+                player.playerElem.height() / 2 +
+                Math.sqrt((player.playerElem.height() ** 2) +
+                    (player.playerElem.width() ** 2))) +
+                (obstacle.obstacleElem.width() / 2)
+            // Calculate the position of the obstacle where the collision is no longer possible
+            const collisionIsNoLongerPossible = Number(player.playerElem.css('left').match(/[0-9\.]+/)[0]) +
+                player.playerElem.height() / 2 -
+                Math.sqrt((player.playerElem.height() ** 2) +
+                    (player.playerElem.width() ** 2))
+            // Get obstacle x position
+            const obstaclePosX = Number(obstacle.obstacleElem.css('left').match(/-?[0-9\.]+/)[0])
+
+            // If an obstacle is in the collision zone
+            const obstacleClose = collisionPosX >= obstaclePosX
+            const obstacleBehind = collisionIsNoLongerPossible > (obstaclePosX + obstacle.obstacleElem.width() / 4)
+            if (obstacleClose && !obstacleBehind) {
+                // Data preparation
+                const obstaclePos = [obstaclePosX, Number(obstacle.obstacleElem.css('top').match(/-?[0-9\.]+/)[0])]
+                const obstacleRadius = obstacle.radius
+                const rectangleSize = [50, Number(player.playerElem.css('height').match(/[0-9]+/)[0])]
+                const rectanglePos = [Number(player.playerElem.css('left').match(/-?[0-9\.]+/)[0]) + rectangleSize[1] / 2, Number(player.playerElem.css('top').match(/-?[0-9\.]+/)[0])]
+                const rectangleRotate = getCurrentRotationInDeg(player.playerElem[0]) - Math.PI / 2
+                // Check collision
+                if (checkOverlap(obstaclePos[0], obstaclePos[1], obstacleRadius, rectanglePos[0], rectanglePos[1], rectangleSize[1] / 2, rectangleSize[0] / 2, rectangleSize[1], rectangleSize[0], rectangleRotate)) {
+                    stopGame()
+                }
+            }
         }
     }
 }
+
 
 
 const animationFrame = () => {
@@ -100,6 +172,9 @@ const animationFrame = () => {
 
     player.physics()
     game.moveBackground()
+    game.generateObstacles()
+    game.moveObstacles()
+    game.checkCollision()
 
     requestAnimationFrame(animationFrame)
     fpsCounter()
@@ -107,7 +182,7 @@ const animationFrame = () => {
 
 
 const game = new Game()
-const player = new Player()
+const player = new Player(20, 0.2)
 
 const startGame = () => {
     game.run = true
@@ -121,6 +196,9 @@ const stopGame = () => {
 
 // On press start button
 playBtn.on('click', () => {
+    // Disable button
+    playBtn.attr("disabled", true)
+
     // Hide menu animation
     startMenu.animate({
         opacity: 0
@@ -142,5 +220,6 @@ playBtn.on('click', () => {
         }
     })
 
-    setTimeout(startGame, 300) // Start game after 300 ms
+    // Start game after 300 ms
+    setTimeout(startGame, 300)
 })
--- scripts/tools.js
+++ scripts/tools.js
@@ -37,14 +37,44 @@ const shiftRelativeToFPS = (data) => {
 
 
 // Function to check if any polet overlaps the given circle and rectangle
-const checkOverlap = (R, Xc, Yc, X1, Y1, X2, Y2) => {
-    // Find the nearest polet on the rectangle to the center of the circle
-    let Xn = Math.max(X1, Math.min(Xc, X2));
-    let Yn = Math.max(Y1, Math.min(Yc, Y2));
-       
-    // Find the distance between the nearest polet and the center of the circle
-    // Distance between 2 polets, (x1, y1) & (x2, y2) in 2D Euclidean space is ((x1-x2)**2 + (y1-y2)**2)**0.5
-    let Dx = Xn - Xc;
-    let Dy = Yn - Yc;
-    return (Dx * Dx + Dy * Dy) <= R * R;
+const checkOverlap = (pointX, pointY, radius, rectX, rectY, rectOffsetX, rectOffsetY, rectWidth, rectHeight, rectAngle) => {
+    const relX = pointX - rectX
+    const relY = pointY - rectY
+    const angle = -rectAngle
+    const angleCos = Math.cos(angle)
+    const angleSin = Math.sin(angle)
+    const localX = angleCos * relX - angleSin * relY
+    const localY = angleSin * relX + angleCos * relY
+    const beyondCorners = Math.sqrt((relX ** 2) + (relY ** 2)) > (Math.sqrt((rectWidth ** 2) + (rectHeight ** 2)) / 2 + radius)
+    return localX >= -rectOffsetX - radius && localX - radius <= rectWidth - rectOffsetX &&
+        localY >= -rectOffsetY - radius && localY <= rectHeight - rectOffsetY + radius && !beyondCorners;
+}
+
+
+// Get current css rotation of element in radians
+const getCurrentRotationInDeg = (el) => {
+    var st = window.getComputedStyle(el, null)
+    var tr = st.getPropertyValue("-webkit-transform") ||
+        st.getPropertyValue("-moz-transform") ||
+        st.getPropertyValue("-ms-transform") ||
+        st.getPropertyValue("-o-transform") ||
+        st.getPropertyValue("transform")
+
+    if (tr !== "none") {
+        var values = tr.split('(')[1]
+        values = values.split(')')[0]
+        values = values.split(',')
+        var a = values[0]
+        var b = values[1]
+
+        return Math.atan2(b, a)
+
+    } else {
+        return 0
+    }
+}
+
+
+const random = (min = 0, max = 100) => {
+    return Math.floor(Math.random() * ((max + 1) - min) ) + min
 }
